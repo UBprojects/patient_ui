29a5f05f8e3f5cb1844b150e75f1606c1e46a84e
Arpit Maheshwari <arp19690@gmail.com>
2019-11-05 20:16:48 -0500

    initial commit

---
+++ patient_ui/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for patient_ui project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+import dotenv
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "patient_ui.settings")
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
+application = get_wsgi_application()
