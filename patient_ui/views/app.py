288caaf462bc1cbbef30a0600cea4a230ff54a13
Arpit Maheshwari <arp19690@gmail.com>
2019-11-25 18:04:16 -0500

    bug fixes

--- patient_ui/views/app.py
+++ patient_ui/views/app.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from helpers import common
 from website.models import *
 
 
@@ -10,10 +9,20 @@ def dashboard(request):
     categories = CategoryPriority.objects.filter(auth_user_id=request.user.id).order_by('order')
     if not categories.exists():
         categories = Category.objects.all().order_by('id')
+    else:
+        exclude_cat_ids = categories.values_list('category_id', flat=True)
+        remaining_cats = Category.objects.exclude(id__in=exclude_cat_ids).order_by('id')
+        if remaining_cats.exists():
+            new_list = list()
+            for cat_priority in categories:
+                new_list.append(cat_priority.category)
+            for rem_cat in remaining_cats:
+                new_list.append(rem_cat)
+            categories = new_list.copy()
 
     result_list = list()
     for cat in categories:
-        subcategories = SubCategory.objects.filter(category=cat).order_by('id')
+        subcategories = SubCategory.objects.filter(category_id=cat.id).order_by('id')
         tmp_dict = {cat.title: list()}
         for subcat in subcategories:
             tmp_dict[cat.title].append({'title': subcat.title})
